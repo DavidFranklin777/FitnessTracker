f499f7198e4c10ee5d4b1de3c21db6f3cbf4b419
DavidFranklin777 <60916144+DavidFranklin777@users.noreply.github.com>
2021-02-28 18:40:55 +0100

    First commit

---
+++ secondwindow.h
@@ -0,0 +1,31 @@
+#ifndef SECONDWINDOW_H
+#define SECONDWINDOW_H
+
+#include <QDialog>
+#include <loginscreen.h>
+#include "DataController.h"
+
+namespace Ui {
+class SecondWindow;
+}
+
+class SecondWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SecondWindow(QWidget *parent = nullptr);
+    ~SecondWindow();
+    void fetchUsername(QString &username);
+
+private slots:
+    void on_pushButton_startTracking_clicked();
+
+    void on_dateEdit_userDateChanged(const QDate &date);
+
+private:
+    Ui::SecondWindow *ui;
+    QString m_Username;
+};
+
+#endif // SECONDWINDOW_H
